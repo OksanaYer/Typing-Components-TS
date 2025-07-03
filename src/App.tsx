d1bec474c8757491ce30f93893abb4bae108aefa
Jorge Paul <jserranopaul@gmail.com>
2025-07-03 12:18:02 +0200

    First comming

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import Container from './layouts/Container';
+
+const App = () => {
+  return <Container style={{ maxWidth: '600px', margin: '0 auto' }}>Hello</Container>;
+};
+
+export default App;
